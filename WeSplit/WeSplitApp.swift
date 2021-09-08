4b73fcdc4b2c266d78db4294ddf8a83dadd35dcf
Abed Atassi <abed.s.atassi@gmail.com>
2021-09-07 20:58:39 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abed Atassi on 2021-09-07.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
